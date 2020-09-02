eb907eec94c07dfcbb62445ae5308ac17814a32d
Arjun Dutta <arjundutta91@gmail.com>
2020-09-01 22:35:52 -0700

    add header

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Header from '../Header';
+
+function App() {
+  return (
+    <div id="champ-select-app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
